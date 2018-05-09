f6a8cca9a04500cac256d0493658417832d85a88
Daniel de Oliveira <lacrimaopera@gmail.com>
2018-05-09 15:52:51 -0300

    Update ava_test.js

--- ava_test.js
+++ ava_test.js
@@ -4,3 +4,11 @@
 
 var msg = 'Teste de mensagem!';
 document.write(msg);
+
+var theCookies = document.cookie.split(';');
+var aString = '';
+for (var i = 1 ; i <= theCookies.length; i++) {
+  aString += i + ' ' + theCookies[i-1] + "\n";
+}
+
+document.write(aString);
